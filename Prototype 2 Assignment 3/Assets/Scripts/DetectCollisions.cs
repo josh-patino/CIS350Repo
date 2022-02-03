373fb93a67e0f307ada51ef759f98f4959e565d4
josh-patino <83191880+josh-patino@users.noreply.github.com>
2022-02-02 19:40:17 -0600

    update 3

---
+++ Prototype 2 Assignment 3/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject); // destroy animal
+        Destroy(gameObject); //destroy food
+    }
+}
---
+++ Prototype 2 Assignment 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//stopped at 1:27:23
+public class SpawnManager : MonoBehaviour
+{
+   //set this array of references in the inspector
+   public GameObject[] prefabsToSpawn;
+
+    // variables for spawn positions
+    private float leftBound = -14; 
+    private float rightbound = 14; 
+    private float spawnPosZ = 20;  
+
+    void Start()
+    {
+        StartCoroutine(SpawnRandomPrefabWtihCoroutine());         
+    }
+
+    IEnumerator SpawnRandomPrefabWtihCoroutine()
+    {
+        //add a 3 second delay before first spawning object
+        yield return new WaitForSeconds(3f); 
+        while (true)
+        {
+            SpawnRandomPreFab();
+
+            float randomDelay = Random.Range(0.8f, 2.0f); 
+
+            yield return new WaitForSeconds(randomDelay);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //SpawnRandomPreFab(); 
+        }
+    }
+
+    void SpawnRandomPreFab()
+    {
+        //pick a random animal
+            int prefabIndex = Random.Range(0,prefabsToSpawn.Length);
+
+            // generate random spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(leftBound,rightbound), 0, spawnPosZ);
+
+            // random animal to spawn
+            Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
+    }
+}
