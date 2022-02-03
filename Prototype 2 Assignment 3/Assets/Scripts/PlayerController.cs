5ce23567a21e86343c4e884471aad633f2b880c6
josh-patino <83191880+josh-patino@users.noreply.github.com>
2022-02-02 18:44:49 -0600

    update 3

---
+++ Prototype 2 Assignment 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput; 
+    public float speed = 10.0f;
+    public float xRange = 14;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal"); 
+
+        //transform horizontally with player input
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+
+        //keep player in bounds on the neg side
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        // keep player in bounds on the pos side
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y , transform.position.z);
+        }
+    }
+}
