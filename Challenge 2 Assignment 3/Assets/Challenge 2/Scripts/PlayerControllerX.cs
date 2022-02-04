1112f775c6cf2498c11a088f91b1a273a5da5aca
josh-patino <83191880+josh-patino@users.noreply.github.com>
2022-02-03 22:11:41 -0600

    update 6

--- Challenge 2 Assignment 3/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge 2 Assignment 3/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float spawnNext = 1.0f;
+    bool spawnDog = true;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // check if spawnDog is true ,On spacebar press, send dog
+        if (spawnDog && Input.GetKeyDown(KeyCode.Space))
         {
+            //spawn dog and set bool to false, start coroutine that contains a timer refering to var spawnNext
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spawnDog = false;
+            StartCoroutine("spawnDogTimer"); 
         }
+
     }
+    //
+    IEnumerator spawnDogTimer()
+    {   //after x time set bool true to spawn dog again this will give a delay of 2 secs by default or more / less if set
+        yield return new WaitForSeconds(spawnNext); //this may be slightly inconsistant but works
+        spawnDog = true;
+    }
+
+
 }
