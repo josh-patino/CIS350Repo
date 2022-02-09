69b55fee5226024ca1bccf2b93cbfbc95227ce97
josh-patino <83191880+josh-patino@users.noreply.github.com>
2022-02-09 00:31:30 -0600

    update 1 prototype 3

---
+++ Prototype 3 Assignment 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.NetworkInformation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb; 
+    public float jumpForce;
+    public bool isOnGround = true; 
+    public ForceMode forceMode; 
+    public bool gameOver = false; 
+    public float gravityModifier; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+        forceMode = ForceMode.Impulse;
+        Physics.gravity *= gravityModifier; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            rb.AddForce(Vector3.up * jumpForce,forceMode); 
+            isOnGround = false; 
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+    }
+
+}
