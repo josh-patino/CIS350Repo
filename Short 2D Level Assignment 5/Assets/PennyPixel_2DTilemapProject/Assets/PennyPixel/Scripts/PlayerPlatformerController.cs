6cb1972f2f6f135b542f479b0ea794b2531ea56e
josh-patino <83191880+josh-patino@users.noreply.github.com>
2022-03-06 22:39:07 -0600

    final 1 min 2D level

---
+++ Short 2D Level Assignment 5/Assets/PennyPixel_2DTilemapProject/Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class PlayerPlatformerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    
+	public int score; 
+	public Text ScoreText; 
+    public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Use this for initialization
+    void Awake () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer> (); 
+        animator = GetComponent<Animator> ();
+    }
+
+    public void AddScore(){
+		score++;
+		ScoreText.text = "Score: " + score.ToString(); 
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag ("Gem")) {
+			AddScore(); 
+			
+		}
+	}
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis ("Horizontal");
+
+        if (Input.GetButtonDown ("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        } else if (Input.GetButtonUp ("Jump")) 
+        {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        if(move.x > 0.01f)
+        {
+            if(spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+        } 
+        else if (move.x < -0.01f)
+        {
+            if(spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+
+        animator.SetBool ("grounded", grounded);
+        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+}
---
+++ Short 2D Level Assignment 5/Assets/endlevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class endlevel : MonoBehaviour
+{
+    public Text endText; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag ("Player")) {
+			endText.text = "You Win!!!";  
+			
+		}
+	}
+}
