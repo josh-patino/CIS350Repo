c7b4ed06b72accd99c162eac923116fd8402e343
josh-patino <83191880+josh-patino@users.noreply.github.com>
2022-04-22 21:56:12 -0500

    Update DestroyObjectX.cs

--- Assignment 8 Challenge 5/Assets/Challenge 5/Scripts/DestroyObjectX.cs
+++ Assignment 8 Challenge 5/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+/*
+* Joshua Patino
+* Assignment 8 Challenge 5
+* destroys the particle attactched to the object after 5 seconds 
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
